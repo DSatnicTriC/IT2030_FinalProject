13138a54657fb6226ee6dcdb279c79993a7290ec
IT2030DSatnic <S01031109@acad.tri-c.edu>
2018-05-06 20:39:42 -0400

    The search results page is a little nicer and now getting a details page for an event that is clicked on

--- FinalProject/Controllers/EventController.cs
+++ FinalProject/Controllers/EventController.cs
@@ -20,12 +20,35 @@ namespace FinalProject.Controllers
 
         public ActionResult Search(string searchEventsQuery, string locationQuery)
         {
-            var matchingEvents = db.Events.Include(e => e.Organizer).Include(e => e.EventType)
-                .Where(x => x.Title.Contains(searchEventsQuery) 
-                            || x.Description.Contains(searchEventsQuery)
-                            || x.Location.Contains(locationQuery))
-                .ToList();
-            return View("SearchResults", matchingEvents);
+            var matchingEvents = db.Events.Include(e => e.Organizer).Include(e => e.EventType);
+            
+            if (string.IsNullOrWhiteSpace(searchEventsQuery) && string.IsNullOrWhiteSpace(locationQuery))
+            {
+                // if the queries are both empty don't filter further (return the whole set)
+            }
+            else if (!string.IsNullOrWhiteSpace(searchEventsQuery) && !string.IsNullOrWhiteSpace(locationQuery))
+            {
+                matchingEvents = matchingEvents.Where(x => (x.Title.Contains(searchEventsQuery)
+                                                                || x.Description.Contains(searchEventsQuery))
+                                                           && x.Location.Contains(locationQuery));
+            }
+            else if (!string.IsNullOrWhiteSpace(searchEventsQuery))
+            {
+                matchingEvents = matchingEvents.Where(x => x.Title.Contains(searchEventsQuery)
+                                                           || x.Description.Contains(searchEventsQuery));
+            }
+            else
+            {
+                matchingEvents = matchingEvents.Where(x => x.Location.Contains(locationQuery));
+            }
+            return View("SearchResults", matchingEvents.ToList());
+        }
+
+        // GET: Event
+        public ActionResult Details(int eventId)
+        {
+            ViewBag.Id = eventId;
+            return View();
         }
     }
 }
